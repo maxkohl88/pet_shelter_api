15413f095fd7f82d60d77fcd49862cc8739347bf
Max Kohl <maxkohl810@gmail.com>
2014-09-18 18:09:01 -0400

    add seed data

---
+++ seed.rb
@@ -0,0 +1,61 @@
+Pet.delete_all
+
+Pet.create(
+id: 1,
+name: "Mr. Snickers",
+species: "Dog",
+breed: "Corgi",
+image_url: "http://upload.wikimedia.org/wikipedia/en/e/e0/Corgi.JPG",
+location: "Cambridge, MA",
+latitude: "42.3736Â° N",
+longitude: "71.1106Â° W")
+
+Pet.create(
+id: 2,
+name: "Lucy",
+species: "Dog",
+breed: "Mutt",
+image_url: "http://www.dogbreedpic.com/wp-content/uploads/2014/01/golden-retriever-chow-mixchow-golden-retriever-mix-mix-breed-dog-photos---all-mutt-opekswdz.jpg",
+location: "Wilton, CT",
+latitude: "73.4333Â° W",
+longitude: "41.2000Â° N")
+
+Pet.create(
+id: 3,
+name: "Snakecat",
+species: "Cat",
+breed: "Weird",
+image_url: "http://www.wallcoo.net/animal/1600_homeless_Cat_03/images/city_cat_5db-003311-x.jpg",
+location: "Santa Monica, CA",
+latitude: "34.0219Â° N",
+longitude: "118.4814Â° W")
+
+Pet.create(
+id: 4,
+name: "Butters",
+species: "Cat",
+breed: "Calico",
+image_url: "http://searchimage.files.wordpress.com/2011/04/calico-cat.jpg",
+location: "Austin, TX",
+latitude: "30.2500Â° N",
+longitude: "97.7500Â° W")
+
+Pet.create(
+id: 5,
+name: "Bosco",
+species: "Dog",
+breed: "St. Bernard",
+image_url: "http://4.bp.blogspot.com/-xEStfrFUTsc/TjAaPnZMGPI/AAAAAAAAMbQ/gFzYRyhrZOE/s640/st+bernard+3.jpg",
+location: "Minneapolis, MN",
+latitude: "44.9833Â° N",
+longitude: "93.2667Â° W")
+
+Pet.create(
+id: 6,
+name: "Dog",
+species: "Dog",
+breed: "Dog",
+image_url: "http://blowmy900.info/wp-content/uploads/2012/01/hello-yes-this-is-dog-original.jpg",
+location: "Seattle, WA",
+latitude: "47.6097Â° N",
+longitude: "122.3331Â° W")
