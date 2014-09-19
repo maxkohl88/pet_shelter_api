b647cfd619292f00ecc6fd4a6bdf1810030769b8
Max Kohl <maxkohl810@gmail.com>
2014-09-19 15:57:07 -0400

    updated seed information

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,8 @@ species: "Dog",
 breed: "Corgi",
 image_url: "http://upload.wikimedia.org/wikipedia/en/e/e0/Corgi.JPG",
 location: "Cambridge, MA",
-latitude: "42.3736Â° N",
-longitude: "71.1106Â° W")
+latitude: "42.3736",
+longitude: "71.1106")
 
 Pet.create(
 id: 2,
@@ -17,8 +17,8 @@ species: "Dog",
 breed: "Mutt",
 image_url: "http://www.dogbreedpic.com/wp-content/uploads/2014/01/golden-retriever-chow-mixchow-golden-retriever-mix-mix-breed-dog-photos---all-mutt-opekswdz.jpg",
 location: "Wilton, CT",
-latitude: "73.4333Â° W",
-longitude: "41.2000Â° N")
+latitude: "73.4333",
+longitude: "41.2000")
 
 Pet.create(
 id: 3,
@@ -27,8 +27,8 @@ species: "Cat",
 breed: "Weird",
 image_url: "http://www.wallcoo.net/animal/1600_homeless_Cat_03/images/city_cat_5db-003311-x.jpg",
 location: "Santa Monica, CA",
-latitude: "34.0219Â° N",
-longitude: "118.4814Â° W")
+latitude: "34.0219",
+longitude: "118.4814")
 
 Pet.create(
 id: 4,
@@ -37,8 +37,8 @@ species: "Cat",
 breed: "Calico",
 image_url: "http://searchimage.files.wordpress.com/2011/04/calico-cat.jpg",
 location: "Austin, TX",
-latitude: "30.2500Â° N",
-longitude: "97.7500Â° W")
+latitude: "30.2500",
+longitude: "97.7500")
 
 Pet.create(
 id: 5,
@@ -47,8 +47,8 @@ species: "Dog",
 breed: "St. Bernard",
 image_url: "http://4.bp.blogspot.com/-xEStfrFUTsc/TjAaPnZMGPI/AAAAAAAAMbQ/gFzYRyhrZOE/s640/st+bernard+3.jpg",
 location: "Minneapolis, MN",
-latitude: "44.9833Â° N",
-longitude: "93.2667Â° W")
+latitude: "44.9833",
+longitude: "93.2667")
 
 Pet.create(
 id: 6,
@@ -57,5 +57,5 @@ species: "Dog",
 breed: "Dog",
 image_url: "http://i2.wp.com/seriouslyguys.com/wp-content/uploads/2013/09/hello-yes-this-is-dog-original.jpg",
 location: "Seattle, WA",
-latitude: "47.6097Â° N",
-longitude: "122.3331Â° W")
+latitude: "47.6097",
+longitude: "122.3331")
