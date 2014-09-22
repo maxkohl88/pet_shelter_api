95424d806d8469ff622b55c33a33a77d40315c7d
Max Kohl <maxkohl810@gmail.com>
2014-09-22 17:23:35 -0400

    changed latitude/longitude data type from integer to decimal

--- db/migrate/20140918175528_create_pets.rb
+++ db/migrate/20140918175528_create_pets.rb
@@ -6,8 +6,8 @@ class CreatePets < ActiveRecord::Migration
       t.string :breed, null: false
       t.string :image_url
       t.string :location, null: false
-      t.integer :latitude
-      t.integer :longitude
+      t.decimal :latitude
+      t.decimal :longitude
 
       t.timestamps
     end
